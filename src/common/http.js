1a436fd8ad0d3841da33e0b5b98fa530e7990f33
lyj <2789771577@qq.com>
2022-04-08 17:31:33 +0800

    git commit -m '第二次提交'

--- src/common/http.js
+++ src/common/http.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export function request(url, params) {
     // 请求超过30秒则判定为超时
     const instance = axios.create({
-        baseURL: '/api',
+        baseURL: process.env.VUE_APP_URL,
         timeout: 30000,
         withCredentials: true,
     });
