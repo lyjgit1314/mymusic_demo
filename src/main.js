8f8702b8fc4a9589cb4e9e4ad0d897254d4dfe13
Sunny <83860971+cordmaker@users.noreply.github.com>
2022-04-08 22:29:43 +0800

    测试自动化部署

--- src/main.js
+++ src/main.js
@@ -7,20 +7,16 @@ import store from './store'
 import './components/flexible'
 
 // 引入element ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 // 引入业务请求api
-import { request } from "./common/http";
+import { request } from './common/http'
 
 // Vue.prototype.$request = request;
 // 将请求挂载在vue原型上 ，可以全局使用
-Vue.prototype.$api = request;
-
-
-
-
+Vue.prototype.$api = request
 
 Vue.config.productionTip = false
 
@@ -28,7 +24,9 @@ new Vue({
   router,
   store,
   render: h => h(App),
-  components:{
-    App  //在 index.html页面不使用这个组件，就会返回 We're sorry but demo3 doesn't work properly without JavaScript enabled. Please enable it to continue.
-   }
+  components: {
+    App, //在 index.html页面不使用这个组件，就会返回 We're sorry but demo3 doesn't work properly without JavaScript enabled. Please enable it to continue.
+  },
 }).$mount('#app')
+
+console.log('测试自动化部署')
